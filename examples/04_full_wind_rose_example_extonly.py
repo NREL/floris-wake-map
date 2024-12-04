d10cec315fa2d969bde4449eb515d3b95fd2b3a1
misi9170 <michael.sinner@nrel.gov>
2024-12-04 14:22:57 -0700

    Example with full wind rose.

---
+++ examples/04_full_wind_rose_example_extonly.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+from floris import FlorisModel
+
+from wakemap import WakeMap
+
+if __name__ == "__main__":
+    with open("/Users/msinner/cluster-wakes_JIP/cw_jip/analysis/wind_rose_vineyard.pkl", "rb") as f:
+        wind_rose_vyw = pickle.load(f)
+        wind_rose_vyw.heterogeneous_map = None
+    wind_rose_vyw.plot()
+
+    #wind_rose_vyw.downsample(wd_step=5.0, ws_step=2.0, inplace=True)
+    #wind_rose_vyw.plot()
+    fig = plt.gcf()
+    fig.savefig("figs/vyw_windrose_full.png", dpi=300, bbox_inches="tight", format="png")
+
+
+    fmodel = FlorisModel("gch.yaml")
+    fmodel.set(turbine_type=["iea_15MW"], reference_wind_height=150.0)
+    nm = 1852
+    x_pos = np.linspace(0, 9*nm, 10)
+    y_pos = x_pos
+    x_pos, y_pos = np.meshgrid(x_pos, y_pos)
+
+    fmodel.set(
+        layout_x=x_pos.flatten(),
+        layout_y=y_pos.flatten(),
+    )
+    print("Conditions in wind rose:", wind_rose_vyw.n_findex)
+    print("Turbines in domain:", len(fmodel.layout_x))
+
+    wakemap = WakeMap(
+        fmodel,
+        wind_rose_vyw,
+        min_dist=nm,
+        group_diameter=6000,
+        bounding_box={"x_min": -10000, "x_max": 25000, "y_min": -10000, "y_max": 25000},
+        external_losses_only=True,
+        verbose=True
+    )
+
+    ax = wakemap.plot_existing_farm()
+    fig = ax.get_figure()
+    fig.savefig("figs/layouts_ex.png", dpi=300, bbox_inches="tight", format="png")
+    ax = wakemap.plot_candidate_locations(ax=ax)
+    fig.savefig("figs/layouts_can.png", dpi=300, bbox_inches="tight", format="png")
+    ax = wakemap.plot_candidate_groups(35, ax=ax)
+    fig.savefig("figs/layouts_groups.png", dpi=300, bbox_inches="tight", format="png")
+
+    wakemap.compute_raw_expected_powers_serial()
+
+    ee = wakemap.process_existing_expected_powers()
+    ce = wakemap.process_candidate_expected_powers()
+
+    # Candidate map (identical, as expected)
+    ax = wakemap.plot_candidate_value(value="capacity_factor")
+    ax = wakemap.plot_existing_farm(ax=ax)
+    ax = wakemap.plot_candidate_locations(ax=ax)
+    ax.set_aspect("equal")
+    fig = ax.get_figure()
+    fig.savefig("figs/candidate_power_map_extonly_vywr_full.png", dpi=300, bbox_inches="tight", format="png")
+
+    # Existing map (differ slightly in shape, magnitude shift. Unsurprising; seems reasonable)
+    ax = wakemap.plot_existing_value(value="capacity_factor")
+    ax = wakemap.plot_existing_farm(ax=ax)
+    ax = wakemap.plot_candidate_locations(ax=ax)
+    ax.set_aspect("equal")
+    fig = ax.get_figure()
+    fig.savefig("figs/existing_power_map_extonly_vywr_full.png", dpi=300, bbox_inches="tight", format="png")
+
+    # Existing map, subset (as for full map).
+    # Make a couple of different options here.
+    subset=range(10)
+    es = wakemap.process_existing_expected_capacity_factors_subset(subset=subset)
+    ax = wakemap.plot_contour(
+        es, cmap="Blues", colorbar_label="Subset turbine capacity factor [-]"
+    )
+    ax = wakemap.plot_existing_farm(ax=ax)
+    ax = wakemap.plot_existing_farm(ax=ax, subset=subset, plotting_dict={"color": "red"})
+    ax = wakemap.plot_candidate_locations(ax=ax)
+    ax.set_aspect("equal")
+    fig = ax.get_figure()
+    fig.savefig("figs/subset_power_map_extonly_vywr_full.png", dpi=300, bbox_inches="tight", format="png")
+
+    plt.show()
